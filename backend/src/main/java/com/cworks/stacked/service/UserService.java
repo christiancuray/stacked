d6fb555896d951e23ed4472a5e5b92e9a1d912fc
Christian Daryl Curay <80902959+christiancuray@users.noreply.github.com>
2025-09-10 15:00:17 -0400

    refractor code

--- backend/src/main/java/com/cworks/stacked/service/UserService.java
+++ backend/src/main/java/com/cworks/stacked/service/UserService.java
@@ -1,26 +1,47 @@
 package com.cworks.stacked.service;
 
+import com.cworks.stacked.repository.RepoUser;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.List;
 
-import com.cworks.stacked.repository.UserRepository;
 import com.cworks.stacked.model.*;
 
 @Service
 public class UserService {
 
-    private final UserRepository userRepository;
+    @Autowired
+    RepoUser userRepository;
 
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
+
+    // add user
+    public User addUser(User user) {
+       return userRepository.save(user);
     }
 
-    public void addUser(User user) {
-        userRepository.addUser(user);
-    }
-
+    // get user by id
     public User getUserById(int id){
-        return userRepository.getUserById(id);
+        return userRepository.findById(id).orElse(null);
     }
+
+
+    // get all users
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    // delete user by id
+    public void deleteUserById(int id) {
+        userRepository.deleteById(id);
+    }
+
+
+    // count users
+    public long countUser(){
+        return userRepository.count();
+    }
+
+
 
 
 }
