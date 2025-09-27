adfe44ddf091ae35738344630564c27138cb173b
Christian Daryl Curay <80902959+christiancuray@users.noreply.github.com>
2025-09-26 23:39:06 -0400

    feat: add signup route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,15 @@ import PostPage from "./components/PostPage";
 import Layout from "./layout/Layout";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
-import { Login } from "./pages/LoginPage";
+import { LoginForm } from "./pages/Login/LoginPage";
+import { SignupForm } from "./pages/SignupPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/signup" element={<SignupForm />} />
         <Route element={<Layout />}>
           <Route index element={<HomePage />} />
           <Route path="/posts" element={<PostPage />} />
