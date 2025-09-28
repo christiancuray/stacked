6372d13b2efd1273514ef9b3e3f0a3c15dfb1c8d
Christian Daryl Curay <80902959+christiancuray@users.noreply.github.com>
2025-09-27 21:24:22 -0400

    feat: add login and signup page

---
+++ frontend/src/pages/SignupPage.tsx
@@ -0,0 +1,135 @@
+import { Button } from "@/components/ui/button";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import z from "zod";
+import axios from "axios";
+
+// define the schema and validation
+const formSchema = z
+  .object({
+    username: z.string().min(1, "username is required"),
+    email: z.string().email("Invalid email address"),
+    password: z.string().min(6, "Password must be at least 6 characters long"),
+    confirmPassword: z
+      .string()
+      .min(6, "Password must be at least 6 characters long"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    // this will check if the passwords are match
+    message: "Passwords do not match!",
+    path: ["confirmPassword"], // set the path of the error to confirmPassword field
+  });
+
+export function SignupForm() {
+  // initialize the form to default values and validation schema
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    // remove the confirmPasword field before sending to backend
+    const { confirmPassword, ...userData } = values;
+    try {
+      const res = await axios.post("/api/users/addUser", userData);
+
+      console.log("Response data:", res.data);
+
+      const user = await res.data;
+      console.log("signup successful:", user);
+      form.reset();
+      if (user) window.location.href = "/login";
+    } catch (err: any) {
+      if (err.response) {
+        alert(err.response.data.message || "Signup failed. Please try again.");
+      } else {
+        alert("An unexpected error occurred.");
+      }
+    }
+  }
+
+  return (
+    <div className="min-h-dvh flex items-center justify-center p-4">
+      <div className="w-full max-w-sm">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="macky" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="macky@gmail.test" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Password" type="password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="confirmPassword"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Confirm Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Confirm Password"
+                      type="password"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" className="w-full">
+              Create Account
+            </Button>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
+}
