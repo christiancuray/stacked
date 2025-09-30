3815cd59d67902eb240d179994705207cd719349
Christian Daryl Curay <80902959+christiancuray@users.noreply.github.com>
2025-09-29 23:11:09 -0400

    feat: add login

--- backend/src/main/java/com/cworks/stacked/api/LoginController.java
+++ backend/src/main/java/com/cworks/stacked/api/LoginController.java
@@ -1,4 +1,40 @@
 package com.cworks.stacked.api;
 
+
+import com.cworks.stacked.model.User;
+import com.cworks.stacked.service.LoginService;
+import com.cworks.stacked.service.UserService;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:5173")
 public class LoginController {
+
+    private final LoginService loginservice;
+
+    @Autowired
+    public LoginController(LoginService loginservice) {
+        this.loginservice = loginservice;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> register(@RequestBody User user){
+        User registeredUser = loginservice.register(user);
+        return ResponseEntity.ok(registeredUser);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Boolean> login(@RequestBody User user) {
+        Boolean isValid = loginservice.login(user);
+        if (Boolean.TRUE.equals(isValid)) {
+            return ResponseEntity.ok(true);
+        } else {
+            return ResponseEntity.status(401).body(false);
+        }
+    }
+
 }
